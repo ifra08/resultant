bac23030a95c3bbe9de9ab0592da6aa338a6208a
ifra08 <irajwadkar@gmail.com>
2017-09-08 13:29:25 +0530

    codeigniter

---
+++ application/views/welcome_message.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Shopping Cart</title>
+    <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css"  type="text/css"/>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css"  type="text/css"/>
+    <!-- Bootstrap CSS -->
+     
+  
+  </head>
+
+<body>
+
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+     <img src="<?= base_url();?>assets/image/logo2.jpg" class="img-responsive" alt="Responsive image" 
+     width="540px"> 
+</div>
+<div class="col-md-8 my-menu">
+      <nav class="navbar navbar-default">
+      <div class="navbar-header">
+      <button type="button" class="navbar-toggle " data-toggle="collapse" data-target="#robust" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      </div>
+      <div class="collapse navbar-collapse" id="robust">
+      <ul class="nav navbar-nav pull-right">
+            <li><a href="#" id="home">Home</a></li>
+            <li><a href="#" id="tech">Technology</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+      <div>
+      </nav>
+</div>
+</div>
+</div>
+
+<div class="container">
+  <div class="row">
+  <img src="<?= base_url();?>assets/image/banner.jpg" class="img-responsive" alt="Responsive image" 
+     > 
+  </div>
+<br>
+
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+
+ <center> <strong class="heading">Welcome To Resultant</strong></center>
+ <br>
+ <br>
+
+ <p  class="text_justify" style="font-size:20px"  data-target="#home">
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  </div>
+  
+</div>
+ 
+<br>
+ <br>
+ <center> <strong class="heading">Welcome To Resultant</strong></center>
+ <br>
+ <br>
+ <div class="row" data-target="#tech">
+  <div class="col-md-4">
+  <div class="well">
+  <img src="<?= base_url();?>assets/image/diploma.jpg" class="img-responsive" alt="Responsive image" 
+     > 
+     
+     <p  class="text_justify" style="font-size:20px">
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+
+   <p class="text-center"> <a class="btn btn-primary" href="#">Read More</a></p>
+  </div>
+  </div>
+
+  <div class="col-md-4">
+  <div class="well">
+  <img src="<?= base_url();?>assets/image/diploma.jpg" class="img-responsive" alt="Responsive image" 
+     > 
+     
+     <p  class="text_justify" style="font-size:20px">
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+
+   <p class="text-center"> <a class="btn btn-primary" href="#">Read More</a></p>
+  </div>
+  </div>
+
+  <div class="col-md-4">
+  <div class="well">
+  <img src="<?= base_url();?>assets/image/diploma.jpg" class="img-responsive" alt="Responsive image" 
+     > 
+     
+     <p  class="text_justify" style="font-size:20px">
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+
+   <p class="text-center"> <a class="btn btn-primary" href="#">Read More</a></p>
+  </div>
+  </div>
+</div>
+</div>
+
+<br>
+<br>
+<div class="row">
+  <div class="col-md-12">
+  
+<br>
+<br>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.540405911239!2d73.02175931499023!3d19.17158498703311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7bf139756a9a9%3A0x7e2014f04471c66e!2sResultant+Academy!5e0!3m2!1sen!2sin!4v1504855220063" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+</div>
+
+<div class="footer">
+  <div class="col-md-4 col-md-offset-4"><center> <strong class="heading">Design By Ifra Rajwadkar</strong></center></div>
+ 
+</div>
+
+<script src="<?php echo base_url();?>assets/js/jquery.js" ></script>
+ <script src="<?php echo base_url();?>assets/js/bootstrap.js" ></script>
+
+  
+</body>
+</html>
